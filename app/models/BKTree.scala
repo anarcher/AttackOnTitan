e68c8f708f8440a70614df764230e33456434f16
msshin <msshin@enswer.net>
2012-07-05 20:47:29 +0900

    add lookup(?) method of BKTree

--- app/models/BKTree.scala
+++ app/models/BKTree.scala
@@ -10,6 +10,21 @@ case class BKTreeNode[T](v:T,c: Map[Int,BKTreeNode[T]] = Map.empty[Int,BKTreeNod
 	case _ => c(d) = BKTreeNode(cv)
     }
   }
+  def ?(cv:T,n:Int=1) : List[(Int,T)] = {
+    val d = Distance(v.toString,cv.toString)
+    val min = math.abs(d - n)
+    val max = d + n
+    println("v=%s,min=%s,max=%s".format(v,min,max))
+    val result = (min to max) flatMap { c.get(_) match {
+	  case Some(x @ BKTreeNode(_v,_)) => {
+	      val d = Distance(cv.toString,_v.toString)
+	      if(d <= n) { (d,_v) :: x ? (cv,n) } else { x ? (cv,n) }
+	  }
+	  case _ => Nil
+       }
+    }
+    result.toList
+  }
 }
 
 class BKTree[T] {
@@ -17,5 +32,9 @@ class BKTree[T] {
   def +(v:T) = node match {
     case Some(x @ BKTreeNode(_,_)) => x + v
     case _ => node = Some(BKTreeNode(v))
+  }
+  def ?(v:T,n:Int=1) = node match {
+    case Some(x @ BKTreeNode(_,_)) => x ? (v,n)
+    case _ => Nil
   }
 }
